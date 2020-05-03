05c84bae81de2d99528b035359663f998d5d8e17
Krishna Yadav <52788412+cryptogeekk@users.noreply.github.com>
2020-05-04 01:16:47 +0530

    Create Feature_extractor.py

---
+++ Feature_extractor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 00:40:56 2020
+
+@author: krishna
+"""
+
+import pandas as pd
+data=pd.read_csv('phishing_dataset.csv') 
+data1=data['URL']
